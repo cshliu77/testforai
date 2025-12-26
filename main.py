5787b30a968e4ec6f414cf18f3e5cb0e5f080829
cshliu <cshliu@aiw.com.tw>
2025-12-26 20:12:49 +0800

    [C2] add additionMethod

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import sys
 def hello_world_en():
     print("Hello, World!")
 def hello_world_zh():
     print("哈囉！世界！")
+def additionMethod(num1, num2):
+    return num1 + num2
+
 if __name__ == '__main__':
-    hello_world_en()
-    hello_world_zh()
+    #check the arguments are enough
+    if len(sys.argv) < 4:
+        print("Not enough arguments.")
+        sys.exit(1)
+        
+    num1 = sys.argv[1]
+    operator = sys.argv[2]
+    num2 = sys.argv[3]
+    
+
+    if operator == '+':
+        print(f"The sum of {num1} and {num2} is {additionMethod(int(num1), int(num2))}")
+    else:
+        print("Unsupported operator.")
+    
