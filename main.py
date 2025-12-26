366dfe84189e1bc56b777d46a217c3214e22cb22
cshliu <cshliu@aiw.com.tw>
2025-12-26 20:07:39 +0800

    [C1]Split Hello World to two func and use os args as input string

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-if __name__ == '__main__':
+def hello_world_en():
     print("Hello, World!")
+def hello_world_zh():
     print("哈囉！世界！")
+if __name__ == '__main__':
+    hello_world_en()
+    hello_world_zh()
